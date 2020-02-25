29052e2ec22d5f2d418c69d5a6e1a901d165bea8
AlejoYarce <alejo.yarce990@gmail.com>
2020-02-25 07:53:31 -0500

    @emotion/styled

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,21 @@
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
+import styled from '@emotion/styled'
+
+const HeaderContainer = styled.header`
+  background-color: rebeccapurple;
+  margin-bottom: 1.45rem;
+`
+const Container = styled.div`
+  margin: 0px auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+  <HeaderContainer>
+    <Container>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
@@ -27,8 +27,8 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
-    </div>
-  </header>
+    </Container>
+  </HeaderContainer>
 )
 
 Header.propTypes = {
